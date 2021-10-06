4013fb5e2024de63b7569d802c3ee60b59b1715c
GustavoQuaresma <gustavo.costa@bandtec.com.br>
2021-10-06 15:02:48 -0300

    conectando com bd

--- learning2/Form1.Designer.cs
+++ learning2/Form1.Designer.cs
@@ -36,8 +36,8 @@ namespace learning2
             this.btnSair = new System.Windows.Forms.Button();
             this.btnDashboard = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblSub = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
@@ -152,8 +152,8 @@ namespace learning2
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblSub);
+            this.panel2.Controls.Add(this.lblName);
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
@@ -161,32 +161,32 @@ namespace learning2
             this.panel2.Size = new System.Drawing.Size(185, 150);
             this.panel2.TabIndex = 0;
             // 
-            // label2
+            // lblSub
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
-            this.label2.Location = new System.Drawing.Point(68, 114);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Subtitle";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.lblSub.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSub.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSub.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
+            this.lblSub.Location = new System.Drawing.Point(0, 118);
+            this.lblSub.Name = "lblSub";
+            this.lblSub.Size = new System.Drawing.Size(185, 15);
+            this.lblSub.TabIndex = 2;
+            this.lblSub.Text = "Subtitle";
+            this.lblSub.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSub.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label1
+            // lblName
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(249)))));
-            this.label1.Location = new System.Drawing.Point(49, 93);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 19);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Username";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblName.AutoEllipsis = true;
+            this.lblName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(249)))));
+            this.lblName.Location = new System.Drawing.Point(0, 91);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(185, 19);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Username";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblName.Click += new System.EventHandler(this.lblName_Click);
             // 
             // pictureBox1
             // 
@@ -229,7 +229,6 @@ namespace learning2
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
@@ -241,8 +240,8 @@ namespace learning2
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnDashboard;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSub;
+        private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button button3;
--- learning2/Form1.cs
+++ learning2/Form1.cs
@@ -13,24 +13,82 @@ using MySql.Data.MySqlClient;
 namespace learning2
 {
     public partial class Form1 : Form
+
     {
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        string strSQL;
+
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
-(
+        (
         int nLeftRect,     // x-coordinate of upper-left corner
         int nTopRect,      // y-coordinate of upper-left corner
         int nRightRect,    // x-coordinate of lower-right corner
         int nBottomRect,   // y-coordinate of lower-right corner
         int nWidthEllipse, // height of ellipse
         int nHeightEllipse // width of ellipse
-);
+         );
         public Form1()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
-        }
+            try
+            {
+                conexao = new MySqlConnection("Server = localhost; Database = logikgames; Uid = root; Pwd = bandtec;");
+                strSQL = "SELECT nome FROM usuario WHERE idUsuario = 1;";
+                comando = new MySqlCommand(strSQL, conexao);
+                conexao.Open();
+                dr = comando.ExecuteReader();
+                while (dr.Read())
+                {
+                    lblName.Text = Convert.ToString(dr["nome"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            try
+            {
+                conexao = new MySqlConnection("Server = localhost; Database = logikgames; Uid = root; Pwd = bandtec;");
+                strSQL = "SELECT estilo FROM usuario WHERE idUsuario = 1;";
+                comando = new MySqlCommand(strSQL, conexao);
+                conexao.Open();
+                dr = comando.ExecuteReader();
+                while (dr.Read())
+                {
+                    switch (dr["estilo"])
+                    {
+                        case 1:
+                            lblSub.Text = "Jogador de League of Legends";
+                            break;
+                        case 2:
+                            lblSub.Text = "Jogador de CS:GO";
+                            break;
+                        case 3:
+                            lblSub.Text = "Jogador de Valorant";
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
 
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -67,6 +125,11 @@ namespace learning2
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
         {
 
         }
