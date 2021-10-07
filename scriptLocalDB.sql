cd79737fd14f65924e291e1a6c0a313531cef536
GustavoQuaresma <gustavo.costa@bandtec.com.br>
2021-10-07 13:06:19 -0300

    inserindo script de banco local

---
+++ scriptLocalDB.sql
@@ -0,0 +1,17 @@
+Create database logikeSports;
+use logikeSports;
+CREATE TABLE usuario (
+    idUsuario int,
+    nome varchar(255),
+    login varchar(255),
+    senha varchar(255),
+    email varchar(255),
+    dataNasc date,
+    Estilo int
+);
+
+insert into usuario values (null, "Gustavo", "admin", "admin123", "asdsada@gmail.com", '2003-3-31',1);
+insert into usuario value (null, "Daniel", "dandan", "1229", "sdads@adsad.com", '2002-2-02',2);
+
+ -- SELECT count(*) FROM usuario WHERE login = 'admin'and senha = 'admin123';
+ -- drop database logikeSports;
